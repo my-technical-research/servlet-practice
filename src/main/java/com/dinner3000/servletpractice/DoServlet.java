9207dec21bbfcc6a85deab1942bc3ea14a6c44b5
Gary.Z <dinner3000@hotmail.com>
2019-02-26 15:32:58 +0800

    init

---
+++ src/main/java/com/dinner3000/servletpractice/DoServlet.java
@@ -0,0 +1,33 @@
+package com.dinner3000.servletpractice;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DoServlet extends HttpServlet {
+
+    private ServletConfig config;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        this.config = config;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        String s = config.getInitParameter("name") + ", GET";
+        resp.getWriter().write(s);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        String s = config.getInitParameter("name") + ", POST";
+        resp.getWriter().write(s);
+    }
+}
